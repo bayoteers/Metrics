f2afa8ce27f441c25ecf2d924f18e95e4477425d
Stephen Jayna <ext-stephen.jayna@nokia.com>
2011-04-25 21:34:20 +0100

    More sensible method of fetching statistics, that by default fetches for every config file under ../conf.

--- contrib/fetch_statistics.sh
+++ contrib/fetch_statistics.sh
@@ -1,16 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
 #==========================================================================
 # BAM: example configuration file
 #
 # Copyright 2011, Nokia Oy
-# Maintainer: Grzegorz Szura <ext-grzegorz.szura@nokia.com>
+# Maintainer: Stephen Jayna <ext-stephen.jayna@nokia.com>
 # Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
-#
-# Date: Thu Feb  3 14:21:00 EET 2011
 #==========================================================================
 
-# Example statistics from meego.com
-fetch_statistics_from_bugzilla.pl /usr/local/etc/BAM/EXAMPLE_MeeGo_Projects.conf
-fetch_statistics_from_bugzilla.pl /usr/local/etc/BAM/EXAMPLE_MeeGo_Projects_-_subset1.conf
-fetch_statistics_from_bugzilla.pl /usr/local/etc/BAM/EXAMPLE_MeeGo_Projects_-_subset2.conf
+
+find ../conf -name "*.conf" -print0 | while read -d $'\0' file
+do
+  echo "../bin/fetch_statistics_from_bugzilla.pl $file"
+done
